ade1d442cc4c06e6f4b34bc8be9ad6c5dc25eba7
VidushiChaudhary <140183903+VidushiChaudhary@users.noreply.github.com>
2025-02-09 22:28:04 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+//Question 1 Example(==)
+let a="5";
+let b=5;
+console.log(a==b);
+
+//Question 1 Example(===)
+let c="10";
+let d=10;
+console.log(c===d);
+
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
+console.log(z);
+
+let e=5;
+let f=6;
+console.log(e+f);
